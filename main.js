36f632e9a6428267069107523512c5784a30a3fd
vilavis <88338820+vilavis@users.noreply.github.com>
2021-08-22 21:36:52 +0200

    Buttons & VW Width
    JS
    - Added Buttons + function "change image source" for the first 2 Paragraphs;
    - Deleted all json files;
    
    CSS
    - Fixed white space on the right and the width bug;
    - Simplified styles for container, wrapper and frame;

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ let genus = document.querySelector('#genus');
 let maskulin = document.querySelector('#maskulin');
 let neutral = document.querySelector('#neutral');
 let feminin = document.querySelector('#feminin');
+let reg = document.querySelector('#reg');
+let unreg = document.querySelector('#unreg');
+let verb = document.querySelector('#verb');
 
 feminin.addEventListener('click', () => {
     genus.src = 'anim/Feminin.gif';
@@ -11,10 +14,14 @@ neutral.addEventListener('click', () => {
     genus.src = 'anim/Neutrum.gif';
 })
 
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('lernen'),
-    renderer: 'svg',
-    loop: false,
-    autoplay: true,
-    path: 'anim/lernen.json'
+maskulin.addEventListener('click', () => {
+    genus.src = 'anim/Maskulin.gif';
+})
+
+reg.addEventListener('click', () => {
+    verb.src = 'anim/Maskulin.gif';
+})
+
+unreg.addEventListener('click', () => {
+    verb.src = 'anim/Feminin.gif';
 })
